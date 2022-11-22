75dfc1e69ce7ee647eb4355cede2896c219f0e10
이세령 <juniorryeong@gmail.com>
2022-11-22 16:35:49 +0900

    codingTest class1 1000~1152

---
+++ src/class1_1000.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class class1_1000 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double A = sc.nextDouble();
+        double B = sc.nextDouble();
+        double result = A / B;
+        System.out.println(result);
+        sc.close();
+    }
+}
---
+++ src/class1_1152.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class class1_1152 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String oneLine = sc.nextLine();
+
+        StringTokenizer st = new StringTokenizer(oneLine, " ");
+        int n = st.countTokens();
+        System.out.println(n);
+        sc.close();
+    }
+}
