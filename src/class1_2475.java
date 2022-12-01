166546dbf4fa02a68deb8dbaa322374f8ba6f849
이세령 <juniorryeong@gmail.com>
2022-12-01 09:13:42 +0900

    codingTest class1 star ~ 2475

---
+++ src/class1_2475.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class class1_2475 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num[] = new int[5];
+        for(int i=0; i<5; i++) {
+            num[i] = sc.nextInt();
+        }
+        int sum = 0;
+        for(int i=0; i<5; i++) {
+            sum += num[i] * num[i];
+        }
+        System.out.println(sum%10);
+        sc.close();
+    }
+}
