1e12b6640128bc6fe0c48dd9afe621ea78d632f7
이세령 <juniorryeong@gmail.com>
2022-12-08 09:04:41 +0900

    codingTest class1 9498 ~ 10171

---
+++ src/class1/class1_10171.java
@@ -0,0 +1,10 @@
+package class1;
+
+public class class1_10171 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
