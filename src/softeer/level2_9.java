62a118120d48f394bb6b442bf19202ec1528eeef
이세령 <juniorryeong@gmail.com>
2023-02-12 21:05:11 +0900

    softeer 2_9

---
+++ src/softeer/level2_9.java
@@ -0,0 +1,20 @@
+package softeer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class level2_9 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        long K = Integer.parseInt(input[0]);
+        long P = Integer.parseInt(input[1]);
+        long N = Integer.parseInt(input[2]);
+        for(int i=0; i<N; i++) {
+            K *= P;
+            K %= 1000000007;
+        }
+        System.out.println(K);
+    }
+}
