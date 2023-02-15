7cc8475f1fe9ebbb9edf07f42d27422e3288cd9e
이세령 <juniorryeong@gmail.com>
2023-02-15 17:31:30 +0900

    level3_2 병합정렬 실패
    - 병합정렬(NlogN) 로 시간초과를 해결해보려 했지만 실패함

--- src/softeer/level3_2.java
+++ src/softeer/level3_2.java
@@ -30,28 +30,54 @@ public class level3_2 {
             finalScore[i] = Integer.parseInt(first[i])+Integer.parseInt(second[i])+Integer.parseInt(third[i]);
             sortedFinal[i] = Integer.parseInt(first[i])+Integer.parseInt(second[i])+Integer.parseInt(third[i]);
         }
-        Arrays.sort(sortedFirst, Collections.reverseOrder());
-        Arrays.sort(sortedSecond, Collections.reverseOrder());
-        Arrays.sort(sortedThird, Collections.reverseOrder());
-        Arrays.sort(sortedFinal, Collections.reverseOrder());
+        Integer[] temp = new Integer[N];
+        mergeSort(0, N-1, sortedFirst, temp);
+        initList(temp);
+        mergeSort(0, N-1, sortedSecond, temp);
+        initList(temp);
+        mergeSort(0, N-1, sortedThird, temp);
+        initList(temp);
+        mergeSort(0, N-1, sortedFinal, temp);
         List<Integer> firstList = new ArrayList<>(Arrays.asList(sortedFirst));
         List<Integer> secondList = new ArrayList<>(Arrays.asList(sortedSecond));
         List<Integer> thirdList = new ArrayList<>(Arrays.asList(sortedThird));
         List<Integer> finalList = new ArrayList<>(Arrays.asList(sortedFinal));
+        printList(firstGame, firstList, N);
+        printList(secondGame, secondList, N);
+        printList(thirdGame, thirdList, N);
+        printList(finalScore, finalList, N);
+    }
+    public static void mergeSort(int start, int end, Integer[] sorted, Integer[] temp) {
+        if (start<end) {
+            int mid = (start+end) / 2;
+            mergeSort(start, mid, sorted, temp);
+            mergeSort(mid+1, end, sorted, temp);
+
+            int p = start;
+            int q = mid + 1;
+            int idx = p;
+
+            while (p<=mid || q<=end) {
+                if (q>end || (p<=mid && sorted[p]>sorted[q])) {
+                    temp[idx++] = sorted[p++];
+                } else {
+                    temp[idx++] = sorted[q++];
+                }
+            }
+            for(int i=start; i<=end; i++) {
+                sorted[i] = temp[i];
+            }
+        }
+    }
+    public static void initList(Integer[] list) {
+        for(int i=0; i<list.length; i++) {
+            list[i] = null;
+        }
+    }
+    public static void printList(Integer[] list, List<Integer> List, int N) {
         for(int i=0; i<N; i++) {
-            System.out.print((firstList.indexOf(firstGame[i])+1) + " ");
+            System.out.print((List.indexOf(list[i])+1) + " ");
         }
         System.out.println();
-        for(int i=0; i<N; i++) {
-            System.out.print((secondList.indexOf(secondGame[i])+1) + " ");
-        }
-        System.out.println();
-        for(int i=0; i<N; i++) {
-            System.out.print((thirdList.indexOf(thirdGame[i])+1) + " ");
-        }
-        System.out.println();
-        for(int i=0; i<N; i++) {
-            System.out.print((finalList.indexOf(finalScore[i])+1) + " ");
-        }
     }
 }
