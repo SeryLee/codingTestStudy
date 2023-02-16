2e1a54686dbd42145b44fa91fa22f9e16d3d15ea
이세령 <juniorryeong@gmail.com>
2023-02-16 17:29:10 +0900

    level3_1 완료, 3_2 작성중
    - PriorityQueue 로 해결할 것

--- src/softeer/level3_2.java
+++ src/softeer/level3_2.java
@@ -6,78 +6,36 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class level3_2 {
+    static int N;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        String[] first = br.readLine().split(" ");
-        String[] second = br.readLine().split(" ");
-        String[] third = br.readLine().split(" ");
-        Integer[] firstGame = new Integer[N];
-        Integer[] sortedFirst = new Integer[N];
-        Integer[] secondGame = new Integer[N];
-        Integer[] sortedSecond = new Integer[N];
-        Integer[] thirdGame = new Integer[N];
-        Integer[] sortedThird = new Integer[N];
-        Integer[] finalScore = new Integer[N];
-        Integer[] sortedFinal = new Integer[N];
-        for(int i=0; i<N; i++) {
-            firstGame[i] = Integer.parseInt(first[i]);
-            sortedFirst[i] = Integer.parseInt(first[i]);
-            secondGame[i] = Integer.parseInt(second[i]);
-            sortedSecond[i] = Integer.parseInt(second[i]);
-            thirdGame[i] = Integer.parseInt(third[i]);
-            sortedThird[i] = Integer.parseInt(third[i]);
-            finalScore[i] = Integer.parseInt(first[i])+Integer.parseInt(second[i])+Integer.parseInt(third[i]);
-            sortedFinal[i] = Integer.parseInt(first[i])+Integer.parseInt(second[i])+Integer.parseInt(third[i]);
-        }
-        Integer[] temp = new Integer[N];
-        mergeSort(0, N-1, sortedFirst, temp);
-        initList(temp);
-        mergeSort(0, N-1, sortedSecond, temp);
-        initList(temp);
-        mergeSort(0, N-1, sortedThird, temp);
-        initList(temp);
-        mergeSort(0, N-1, sortedFinal, temp);
-        List<Integer> firstList = new ArrayList<>(Arrays.asList(sortedFirst));
-        List<Integer> secondList = new ArrayList<>(Arrays.asList(sortedSecond));
-        List<Integer> thirdList = new ArrayList<>(Arrays.asList(sortedThird));
-        List<Integer> finalList = new ArrayList<>(Arrays.asList(sortedFinal));
-        printList(firstGame, firstList, N);
-        printList(secondGame, secondList, N);
-        printList(thirdGame, thirdList, N);
-        printList(finalScore, finalList, N);
-    }
-    public static void mergeSort(int start, int end, Integer[] sorted, Integer[] temp) {
-        if (start<end) {
-            int mid = (start+end) / 2;
-            mergeSort(start, mid, sorted, temp);
-            mergeSort(mid+1, end, sorted, temp);
-
-            int p = start;
-            int q = mid + 1;
-            int idx = p;
-
-            while (p<=mid || q<=end) {
-                if (q>end || (p<=mid && sorted[p]>sorted[q])) {
-                    temp[idx++] = sorted[p++];
-                } else {
-                    temp[idx++] = sorted[q++];
-                }
+        N = Integer.parseInt(br.readLine());
+        int[] totalScore = new int[N];
+        for(int i=0; i<3; i++) {
+            String[] game = br.readLine().split(" ");
+            PriorityQueue<Gamer> priorityQueue = new PriorityQueue<>();
+            for(int j=0; j<N; j++) {
+                int score = Integer.parseInt(game[j]);
+                priorityQueue.offer(new Gamer(j, score));
             }
-            for(int i=start; i<=end; i++) {
-                sorted[i] = temp[i];
-            }
+
         }
     }
-    public static void initList(Integer[] list) {
-        for(int i=0; i<list.length; i++) {
-            list[i] = null;
-        }
+    public static int[] getGrade(PriorityQueue<Gamer> pq) {
+
+        int[] grades = new int[N];
+        return grades;
     }
-    public static void printList(Integer[] list, List<Integer> List, int N) {
-        for(int i=0; i<N; i++) {
-            System.out.print((List.indexOf(list[i])+1) + " ");
+    static class Gamer implements Comparable<Gamer>{
+        int idx;
+        int score;
+        public Gamer(int idx, int score) {
+            idx = this.idx;
+            score = this.score;
         }
-        System.out.println();
+        @Override
+        public int compareTo(Gamer o) {
+            return o.score - this.score;
+        }
     }
 }
