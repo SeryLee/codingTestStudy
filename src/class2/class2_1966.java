2edc3cb3923c386193e3088f7d9c688b6d606fd4
이세령 <juniorryeong@gmail.com>
2023-02-23 22:14:56 +0900

    class2_1966 실패

--- src/class2/class2_1966.java
+++ src/class2/class2_1966.java
@@ -1,10 +1,10 @@
 package class2;
 
+import javax.imageio.ImageTranscoder;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 public class class2_1966 {
     public static void main(String[] args) throws IOException {
@@ -12,37 +12,51 @@ public class class2_1966 {
         int T = Integer.parseInt(br.readLine());
         for(int i=0; i<T; i++) {
             String[] input = br.readLine().split(" ");
+            String[] print = br.readLine().split(" ");
             int N = Integer.parseInt(input[0]);
             int M = Integer.parseInt(input[1]);
-            String[] print = br.readLine().split(" ");
-            Queue[] printOrder = new Queue[10];
-            Queue<Integer> nowPrintList = new LinkedList<>();
+            Queue[] priority = new Queue[10];
+            Queue<Integer> printList = new LinkedList<>();
+            Queue<Integer> index = new LinkedList<>();
+            List<Integer> tempList = new ArrayList<>();
             int max = 0;
             for(int j=0; j<N; j++) {
-                nowPrintList.offer(Integer.parseInt(print[j]));
+                printList.offer(Integer.parseInt(print[j]));
+                index.offer(j);
+                tempList.add(Integer.parseInt(print[j]));
                 max = Math.max(max, Integer.parseInt(print[j]));
             }
-            int idx = 0;
-            while(!nowPrintList.isEmpty()) {
-                int nowPrint = nowPrintList.poll();
-                if(idx == N) idx = 0;
-                if(nowPrint != max && idx <N) {
-                    nowPrintList.offer(nowPrint);
-                    idx++;
-                } else if(nowPrint == max && idx < N) {
-                    if(printOrder[nowPrint] == null) {
-                        Queue<Integer> nowIndexPrint = new LinkedList<>();
-                        nowIndexPrint.offer(idx);
-                        printOrder[nowPrint] = nowIndexPrint;
+            Set<Integer> sort = new HashSet<>(tempList);
+            List<Integer> existP = new ArrayList<>(sort);
+            Collections.sort(existP);
+            max = existP.get(existP.size()-1);
+            while (max>0) {
+                Queue<Integer> tempQ = new LinkedList<>();
+                while(printList.size() != 0) {
+                    int nowPrint = printList.poll();
+                    int nowIdx = index.poll();
+                    if(nowPrint == max) {
+                        if(priority[nowPrint] == null) {
+                            Queue<Integer> temp = new LinkedList<>();
+                            temp.offer(nowIdx);
+                            priority[nowPrint] = temp;
+                        } else {
+                            priority[nowPrint].offer(nowIdx);
+                        }
                     } else {
-                        printOrder[nowPrint].offer(idx);
+                        tempQ.offer(nowPrint);
+                        index.offer(nowIdx);
                     }
+                }
+                max--;
+                for (Integer integer : tempQ) {
+                    printList.offer(integer);
                 }
             }
             int cnt = 1;
             for(int j=9; j>=0; j--) {
-                while(printOrder[j] != null && !printOrder[j].isEmpty()) {
-                    int nowIdx = (int) printOrder[j].poll();
+                while(priority[j] != null && !priority[j].isEmpty()) {
+                    int nowIdx = (int) priority[j].poll();
                     if(nowIdx == M) {
                         System.out.println(cnt);
                         break;
