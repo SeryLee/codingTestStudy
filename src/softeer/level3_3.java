ea13afb1409a98b9fb125f83c7579eb7c7bc2207
이세령 <juniorryeong@gmail.com>
2023-02-25 22:10:37 +0900

    level3_3 작성중

---
+++ src/softeer/level3_3.java
@@ -0,0 +1,11 @@
+package softeer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class level3_3 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    }
+}
