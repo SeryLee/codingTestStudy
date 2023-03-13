1ada3886f78fadd8c453f492b2a68e3efe2128a2
이세령 <juniorryeong@gmail.com>
2023-03-13 22:07:45 +0900

    class2_2798 실패
    - sort 후 연속된 세 개의 수를 합한 값으로 하려 했으나 케이스 1번은 통과, 2번은 실패

--- src/class2/class2_2798.java
+++ src/class2/class2_2798.java
@@ -1,7 +1,47 @@
 package class2;
 
-public class class2_2798 {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
+public class class2_2798 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int N = Integer.parseInt(input[0]);
+        int M = Integer.parseInt(input[1]);
+
+        String[] card = br.readLine().split(" ");
+        List<Integer> cardList = new ArrayList<>();
+        for(int i=0; i<N; i++) {
+            cardList.add(Integer.parseInt(card[i]));
+        }
+
+        Collections.sort(cardList);
+        int searchNum = N / 2;
+        int first = 0;
+        int end = 0;
+        int mid = 0;
+        int result = 0;
+        while (searchNum < N-1 && searchNum > 0) {
+            first = cardList.get(searchNum - 1);
+            mid = cardList.get(searchNum);
+            end = cardList.get(searchNum + 1);
+            if((first+mid+end) == M) {
+                result = (first + mid + end);
+                break;
+            } else if((first+mid+end) < M) {
+                result = Math.max((first + mid + end), result);
+                searchNum++;
+            } else {
+                if(result == 0) {
+                    searchNum--;
+                }
+            }
+        }
+        System.out.println(result);
     }
 }
