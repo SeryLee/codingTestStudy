0a2c9075aa07f8a20e6d01935cc5b2df2f3a3645
이세령 <juniorryeong@gmail.com>
2023-03-31 08:49:50 +0900

    programmers 전화번호 목록 실패

--- src/programmers/전화번호_목록.java
+++ src/programmers/전화번호_목록.java
@@ -9,21 +9,24 @@ public class 전화번호_목록 {
         Collections.sort(pList);
         Map<Integer, Integer> map = new HashMap<>();
         int cnt = 0;
+        int maxl = 0;
         for(int i=0; i<pList.size(); i++) {
             if(pList.get(i).length() > cnt) {
                 cnt = pList.get(i).length();
                 map.put(cnt, i);
+                maxl = Math.max(maxl, cnt);
             }
         }
         if(map.size() > 1) {
             int nextIdx = 0;
-            for(int i=0; i<pList.size(); i++) {
-                String s = pList.get(i);
-                if(map.get(s.length()+1) != null) {
-                    nextIdx = map.get(s.length()+1);
-                    for(int j=nextIdx; j<pList.size(); j++) {
-                        if(pList.get(j).startsWith(s)) answer = false;
-                        break;
+            for(int i=0; i<map.get(maxl); i++) {
+                for(int h : map.keySet()) {
+                    if(h>pList.get(i).length()) {
+                        nextIdx = map.get(h);
+                        for(int j=nextIdx; j<pList.size(); j++) {
+                            if(pList.get(j).startsWith(pList.get(i))) answer = false;
+                            break;
+                        }
                     }
                 }
                 if(!answer) break;
