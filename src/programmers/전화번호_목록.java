7df49719fc58e6e882094fcda36551d6cc4e2c50
이세령 <juniorryeong@gmail.com>
2023-04-01 13:28:58 +0900

    programmers 전화번호 목록 완성

--- src/programmers/전화번호_목록.java
+++ src/programmers/전화번호_목록.java
@@ -5,31 +5,13 @@ import java.util.*;
 public class 전화번호_목록 {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
-        List<String> pList = new ArrayList<>(Arrays.asList(phone_book));
-        Collections.sort(pList);
-        Map<Integer, Integer> map = new HashMap<>();
-        int cnt = 0;
-        int maxl = 0;
-        for(int i=0; i<pList.size(); i++) {
-            if(pList.get(i).length() > cnt) {
-                cnt = pList.get(i).length();
-                map.put(cnt, i);
-                maxl = Math.max(maxl, cnt);
-            }
+        Map<String, Integer> map = new HashMap<>();
+        for(String s : phone_book) {
+            map.put(s, s.length());
         }
-        if(map.size() > 1) {
-            int nextIdx = 0;
-            for(int i=0; i<map.get(maxl); i++) {
-                for(int h : map.keySet()) {
-                    if(h>pList.get(i).length()) {
-                        nextIdx = map.get(h);
-                        for(int j=nextIdx; j<pList.size(); j++) {
-                            if(pList.get(j).startsWith(pList.get(i))) answer = false;
-                            break;
-                        }
-                    }
-                }
-                if(!answer) break;
+        for(int i=0; i<phone_book.length; i++) {
+            for(int j=0; j<phone_book[i].length(); j++) {
+                if(map.containsKey(phone_book[i].substring(0, j))) return false;
             }
         }
         return answer;
